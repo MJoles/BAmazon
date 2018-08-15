1c4252e7144e4d72b194cd2821edcf3b260672d6
Mandy <mandyjjoles@gmail.com>
2018-08-15 18:20:20 -0500

    prompts working with table showing all products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer")
+var table = require("console.table")
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -18,5 +19,36 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    
+   
+    allProducts();
 });
+
+function allProducts() {
+    // query the database for all items for sale
+    connection.query("SELECT * from products;", function(err, results, fields) {
+        if (err) throw err;
+        else {
+        // console log all products
+        console.table(results);
+        
+      }
+      pickProduct();
+    }
+    
+)}
+
+function pickProduct() {
+    inquirer
+      .prompt([
+        {
+        name: "pickProduct",
+        type: "input",
+        message: "What is the item_id of the product you would like to buy?"
+      },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to buy?"
+    }])
+
+};
