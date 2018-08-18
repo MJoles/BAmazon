8936e7ec4dc5ea8942e42ee150c01422479640e4
Mandy <mandyjjoles@gmail.com>
2018-08-17 21:52:38 -0500

    check if quantity is in stock for order working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    console.log("Welcome to BAmazon! Take a look at our products for sale below!");
    
     allProducts();
 });
@@ -33,22 +33,46 @@ function allProducts() {
         
       }
       pickProduct();
+     
     }
     
 )}
 
 function pickProduct() {
     inquirer
-      .prompt([
+        .prompt([
         {
-        name: "pickProduct",
-        type: "input",
-        message: "What is the item_id of the product you would like to buy?"
-      },
-    {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to buy?"
-    }])
+          name: "product",
+          type: "input",
+          message: "What is the item_id of the product you would like to buy?"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to buy?"
+        }
+        ])
+        .then(function(answer) {
+            console.log("Your order has been placed!");
+            console.log(answer);
+            var product = answer.product;
+            var quantity = answer.quantity;
+            
+            var queryProducts = "SELECT * FROM products WHERE ?";
+            connection.query(queryProducts, {item_id: product}, function(err, res) {
+                if (err) throw err;
+                 else {
+                    var productInfo = res[0];
+                    if (quantity <= productInfo.stock_quantity) {
+                        console.log("The product you want is in stock!")
+                    }
 
+                }
+            })
+        })
+    
 };
+
+function checkProduct() {
+    var query = connection.query("SELECT")
+}
