be1c0b32f226feb22078d31fec51d150845bf2fc
Mandy <mandyjjoles@gmail.com>
2018-08-19 01:08:29 -0500

    Final commit with prompt for second order and end connection including screenshots folder added to display working application

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BAmazon
 Node.js and MySQL - Homework
+
+This app BAmazon is a using a MySQL database and node to query the database and place orders using prompts, user input, and updating tables.
+
+See first screenshot from the application showing the table of products available for purchase. This used the console.table npm package to display the table of products from MySQL database. Then uses the inquirer npm package to prompt user. If the user chooses a larger quantity than what is in stock, the user must choose a different quantity.
+/Screenshots/insufficientQuantity.PNG
+
+Second screenshot displays a proper order with sufficient quantities being placed which uses the mysql npm package to query the database. Calculates cost with quantity order and returns total cost to the console.
+/Screenshots/orderPlaced.PNG
+
+Third screenshot is when the user is prompted to start a new order. If selected yes, all products for sale will display and be prompted to select again. If selected no, connection to database ends and console will display thanks for shopping.
+/Screenshots/nextOrder.PNG
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer")
-var table = require("console.table")
+var mysql = require("mysql");;
+var inquirer = require("inquirer");
+var table = require("console.table");
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -61,23 +62,32 @@ function pickProduct() {
             var queryProducts = "SELECT * FROM products WHERE ?";
             var cost 
             connection.query(queryProducts, {item_id: product}, function(err, res) {
+                var productInfo = res[0];
                 if (err) throw err;
+                if (quantity > productInfo.stock_quantity) {
+                    console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    console.log("I'm sorry we don't have enough in stock, choose a smaller quantity!");
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
+                    allProducts()
+                    
+                }
                 
                  else {
-                    var productInfo = res[0];
+                   
                     if (quantity <= productInfo.stock_quantity) {
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                        console.log("We have " + productInfo.stock_quantity + " " + productInfo.product_name + "s in stock for your order!")
+                        console.log("We have " + quantity + " " + productInfo.product_name + "s in stock for your order!")
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                         console.log("Thank you for your order! Please wait while we process your order!");
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
-                    };
+                    } 
                     if (cost = quantity * productInfo.price) {
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                         console.log("The total cost of your order is $" + cost + ".00");
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
                     }
+                    
             var queryUpdate = "UPDATE products SET ? WHERE ?"
             connection.query(queryUpdate, [{stock_quantity: answer.quantity},{item_id: product}], function(err, res) {
                  if (err) throw err;
@@ -85,17 +95,39 @@ function pickProduct() {
                     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");  
                     console.log("Inventory has been updated!");
                     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
-                        allProducts();
+                                   
+                  
+                   inquirer
+                   .prompt({
+                    name: 'next',
+                    type: "input",
+                    message: 'Would you like to place another order (Yes/No)?',
+                    })
+                  .then(function(answer) {
+                      if (answer.next === "Yes") {
+                          allProducts();
+                      } else {
+                        connection.end()
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        console.log("Thank you for shopping with us! Come back soon!")
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                      }
+                    
+                  });
+                   
+                    
                       }
                 })
                     }
 
                 
             })
+         
+        })
 
         
-        })
+        }
     
-};
+
 
 
